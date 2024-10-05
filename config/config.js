329554f2ea4a8f8d9f6ca5815f2dcdf4f2bdaf6f
Brandon <14009481+bng94@users.noreply.github.com>
2024-10-05 09:43:53 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,2 @@
 const dev = process.env.NODE_ENV !== "production";
-export const SERVER = dev
-  ? "http://localhost:3000"
-  : "https://nextjs-portfolio-bng94.vercel.app/";
+export const SERVER = dev ? "http://localhost:3000" : "https://ngbrandon.com";
